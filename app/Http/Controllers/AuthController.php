968b338970df6658cc230f64160173c1cad2d5c2
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-08-17 13:06:57 +0500

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,19 +16,38 @@ class AuthController extends Controller
     {
 
         $request->validate([
-            'name' => 'required|string',
+            'name' => 'string',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string'
         ]);
+
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
         $user->save();
+        $credentials = request(['email', 'password']);
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        $user = $request->user();
+        $tokenResult = $user->createToken((string)Str::uuid());
+        $token = $tokenResult->token;
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
+        $user->token= $tokenResult->accessToken;
+        $user->save();
         return response()->json([
-            'message' => 'Successfully created user!'
-        ], 201);
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ],201);
+
     }
     public function login(Request $request)
     {
@@ -74,7 +93,9 @@ class AuthController extends Controller
      */
     public function user(Request $request)
     {
-        return response()->json($request->user());
+        $user=$request->user();
+
+        return response()->json(['user'=>$user]);
     }
 
 }
